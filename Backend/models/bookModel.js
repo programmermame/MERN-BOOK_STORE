8bf7dbccfff8d0c1162fbd238277bd800096fb26
programmermame <mameatpickout@gmail.com>
2025-03-08 15:02:08 +0300

    Create Book Model

---
+++ Backend/models/bookModel.js
@@ -0,0 +1,32 @@
+import mongoose, { mongo } from "mongoose";
+
+const bookSchema = mongoose.Schema({
+    ISBN: {
+        type: Number,
+        required: true,
+    },
+    Title: {
+        type: String,
+        required: true,
+    },
+    Author: {
+        type: String,
+        required: true,
+    },
+    Genre: {
+        type: String,
+        required: true,
+    },
+    PublicationDate: {
+        type: Date,
+        required: true,
+    }
+},
+    {
+        timestamps: true,
+    }
+);
+
+const bookModel = mongoose.model("Book", bookSchema);
+
+export default bookModel;
