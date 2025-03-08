f4b3bf878695960dad99019bdecf1aecc54de989
programmermame <mameatpickout@gmail.com>
2025-03-08 14:59:02 +0300

    Set up Express server

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
+import "dotenv/config";
+import connectDatabase from './config/database.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 
 
 
 
-
+connectDatabase();
 
 app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`);
