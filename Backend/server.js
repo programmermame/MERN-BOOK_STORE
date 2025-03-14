dfa607836f2a41e3c711cda646d4563726238634
programmermame <mameatpickout@gmail.com>
2025-03-14 23:03:58 +0300

    Enable JSON parsing & add book routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import "dotenv/config";
 import connectDatabase from './config/database.js';
+import bookRoutes from './routes/bookRoutes.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-
+app.use(express.json());
+app.use('/api/v1/', bookRoutes);
 
 
 
